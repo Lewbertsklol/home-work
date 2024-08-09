810d6a7de6bb0a61082c2fe435f50b75ba9b4d74
Lewbertsklol <lewbertsklol@gmail.com>
2024-08-09 13:45:59 +0700

    HW7

--- apps/catalog/views/product.py
+++ apps/catalog/views/product.py
@@ -1,8 +1,6 @@
-from django.forms import BaseModelForm
-from django.http import HttpResponse
 from django.views import generic
 from django.urls import reverse_lazy
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from apps.catalog.models import Product
 from apps.catalog.forms import ProductForm
 from apps.users.permissions import EmailVerifiedRequiredMixin
@@ -12,21 +10,21 @@ class ProductListView(generic.ListView):
     model = Product
 
 
-class ProductCreateView(EmailVerifiedRequiredMixin, generic.CreateView):
+class ProductCreateView(LoginRequiredMixin, EmailVerifiedRequiredMixin, generic.CreateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('catalog:product_list')
-    
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         form.save()
         return super().form_valid(form)
 
 
-class ProductDetailView(EmailVerifiedRequiredMixin, generic.DetailView):
+class ProductDetailView(LoginRequiredMixin, EmailVerifiedRequiredMixin, generic.DetailView):
     model = Product
 
 
-class ProductDeleteView(EmailVerifiedRequiredMixin, generic.DeleteView):
+class ProductDeleteView(LoginRequiredMixin, EmailVerifiedRequiredMixin, generic.DeleteView):
     model = Product
     success_url = reverse_lazy('catalog:product_list')
